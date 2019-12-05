1a8d91a23b642c07e34d04211ebbe2e7e763b4d2
shikher <sirshikher@gmail.com>
2019-12-05 14:02:48 +0530

    Initial Phase :trollface

---
+++ single-spa.config.js
@@ -0,0 +1,33 @@
+import { registerApplication, start } from 'single-spa'
+
+function pathPrefix(prefix) {
+  return function(location) {
+      return location.pathname.startsWith(prefix);
+  }
+}
+
+
+registerApplication(
+  // Name of our single-spa application
+  'home',
+  // Our loading function
+  () => import('./src/home/home.app.js'),
+  // Our activity function
+  () => location.pathname === "" || 
+    location.pathname === "/" || 
+    pathPrefix('/home')
+);
+registerApplication(
+  'navBar', 
+
+  () => import('./src/navBar/navbar.app.js').then(module => module.navBar),  () => true
+);
+// registerApplication(
+//   'angularJS', 
+//   () => import ('./src/angularJS/angularJS.app.js'), 
+//   pathPrefix('/angularJS')
+// );
+
+
+
+start();
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+
+module.exports = {
+  mode: 'development',
+  // entry: ["babel-polyfill",'./single-spa.config.js'],
+
+  entry: {
+    'single-spa.config': './single-spa.config.js',
+  },
+  output: {
+    publicPath: '/dist/',
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ng$/,
+        use: [ 'ng-loader' ]
+      },
+      {
+        test: /\.js(x)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }]},
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }, {
+        test: /\.js$/,
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },  {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          },
+        ],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+          
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: [{ loader: 'file-loader?name=/home/nineleaps/Documents/micro-frontnd/micro-frontend-example/src/home/assets/img/[name].[ext]', options: {} }],
+      },
+      
+      
+    ],
+  },
+  node: {
+    fs: 'empty'
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    },
+    modules: [path.resolve(__dirname, 'node_modules')],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({template: './index.html'})
+  ],
+  devtool: 'source-map',
+  externals: [],
+  devServer: {
+    historyApiFallback: true
+  }
+};
